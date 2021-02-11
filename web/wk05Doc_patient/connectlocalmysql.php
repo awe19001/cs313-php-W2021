74a6299d039c5b238bd69e0a8ddb7dbffd793d94
Eve Awe <joeverlyncapute@gmail.com>
2021-02-10 19:00:35 -0700

    local or heroku connect

--- web/wk05Doc_patient/connectlocalmysql.php
+++ web/wk05Doc_patient/connectlocalmysql.php
@@ -1,12 +1,12 @@
 <?php
 
-//connect mysql
+//connect heroku
 try {
     // Set Database Credentials
-    $dbHost = 'ec2-34-192-72-159.compute-1.amazonaws.com';
-    $dbName = 'd7a3jfu7vso8bu';
-    $dbUser = 'ekemdhtwjhvcnz';
-    $dbPassword = '712b50f2ad63bd3e6bbd75a6322fd274fc6a84dd24d7525965a9e39da88a4bfb';
+    $dbHost = 'localhost';
+    $dbName = 'XXXXXXXXXX';
+    $dbUser = 'iClient';
+    $dbPassword = 'XXXXXXXXXX';
     // Tell PDO to give us exception errors for debugging in needed
     $dbOptions = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
     // Create the PDO connection for MySQL
