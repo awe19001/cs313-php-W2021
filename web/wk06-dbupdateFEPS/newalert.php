a75d6db761c1803ef33749e32bc77e1fb001f91c
Eve Awe <joeverlyncapute@gmail.com>
2021-02-13 10:06:50 -0700

    ad

--- web/wk06-dbupdateFEPS/newalert.php
+++ web/wk06-dbupdateFEPS/newalert.php
@@ -37,7 +37,25 @@
 -- Chaka Khan
 </div>
 
-<h3> See list of events here! </h3>
+
+<div> 
+<form> 
+  <p>First name: <input type="text" id="fname" name ="firstname"></p>
+  <p>Last name: <input type="text" id="lname" name="lastname"></p>
+  <p><input type="button" value="Submit" id="namebutton"></p>
+  <script>
+    // Just get a reference to the element
+    var fname = document.getElementById("fname");
+    var lname = document.getElementById("lname");
+    document.getElementById("namebutton").addEventListener("click", function () { 
+      // Then, use the properties of the (already gotten) elment as you need them:
+      alert('Hello ' + fname.value + " " + lname.value);
+    })
+  </script>
+</form>
+</div>
+
+<h3> See list of family events here! </h3>
 <div>
     <p>
 	 <a href="#">	New Year </a> </br>
