66ea343b3e2708d2c005f9f8434c38ce95616369
Eve Awe <joeverlyncapute@gmail.com>
2021-02-13 02:13:02 -0700

    inserting note

--- web/wk06-demo/insert_note.php
+++ web/wk06-demo/insert_note.php
@@ -3,6 +3,19 @@
 $course_id = htmlspecialchars($_POST['course_id']);
 $content = htmlspecialchars($_POST['note_content']);
 
-echo "$course_id\n";
-echo $content;
+require('dbConnect.php');
+$db = get_db();
+
+// From the reading wk05
+//query statement
+$stmt = $db->prepare('INSERT INTO note(course_id, content) VALUES (:course_id, :content);');
+$stmt->bindValue(':id', $course_id, PDO::PARAM_INT);
+$stmt->bindValue(':content', $content, PDO::PARAM_STR);
+$stmt->execute();
+
+$new_page = "course_notes.php?id=$course_id";
+
+header("Location: $new_page");
+die();
+
 ?>
