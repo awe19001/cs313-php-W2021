99470620feee96885f3e388db71c560749193db0
Eve Awe <joeverlyncapute@gmail.com>
2021-02-13 08:09:10 -0700

    href

--- web/wk06-dbupdateFEPS/landing.php
+++ web/wk06-dbupdateFEPS/landing.php
@@ -26,6 +26,7 @@ $landings = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="landingstyle.css">
     <title>Landing Page</title>
 </head>
 <body>
