5517b7ce50f3212f96234ab21497f7df7638fc2c
Eve Awe <joeverlyncapute@gmail.com>
2021-02-13 03:50:19 -0700

    login form

---
+++ web/wk06-dbupdateFEPS/login.php
@@ -0,0 +1,41 @@
+<?php
+require('dbconnect.php');
+$db = get_db();
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Register</title>
+</head>
+<body>
+
+<div class="container" style="width: 500px; height: 240px; border: 2px solid red; margin-top: 3px;">
+    <h2 class="text-center">Welcome to Family Even Planning System</h2>
+
+    <form method="POST">
+       <label>Username</label>
+       <input type="text" name="username" class="form-control" />
+       <br />
+       <label>Password</label>
+       <input type="password" name="password" class="form-control" />
+       <br />
+       <input type="submit" name="login" class="btn btn-primary btn-block" value="Login" />
+      
+    </form>
+</div>
+<br />
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+</body>
+</html>
