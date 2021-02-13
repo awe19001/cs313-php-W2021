83a80d31ff302e958d8e1255d76ccad079dd2731
Eve Awe <joeverlyncapute@gmail.com>
2021-02-13 08:02:51 -0700

    css

--- web/wk06-dbupdateFEPS/login.php
+++ web/wk06-dbupdateFEPS/login.php
@@ -13,14 +13,15 @@ $db = get_db();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="loginstyle.css">
     <title>Login</title>
 </head>
 <body>
 
-<div class="container" style="width: 500px; height: 240px; border: 2px solid red; margin-top: 3px;">
+<div class="container">
     <h2 class="text-center">Welcome to Family Event Planning System</h2>
 
-    <form method="POST" action="login.php">
+    <form method="POST" action="landing.php">
        <label>Username</label>
        <input type="text" name="username" class="form-control" />
        <br />
