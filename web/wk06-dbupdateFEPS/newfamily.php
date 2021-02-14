ee12f80762edf35c25e19f294540fb1d8097390b
Eve Awe <joeverlyncapute@gmail.com>
2021-02-13 17:10:02 -0700

    'saf

--- web/wk06-dbupdateFEPS/newfamily.php
+++ web/wk06-dbupdateFEPS/newfamily.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="newfamily.css">
     <title>New Family Form</title>
 </head>
 <body>
 
-<div class="container" style="width: 500px; height: 500px; border: 2px solid red; margin-top: 3px;">
+<div class="container">
     <h2 class="text-center">New Family Form</h2>
 
     <form method="POST" >
