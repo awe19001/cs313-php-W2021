076d764af3935963da1db4dd82563fd653055857
Eve Awe <joeverlyncapute@gmail.com>
2021-02-13 17:02:59 -0700

    saf

--- web/wk06-dbupdateFEPS/form.php
+++ web/wk06-dbupdateFEPS/form.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="form.css">
     <title>Create Forms</title>
 </head>
 
 
 <body>
-<div class="container" style="width: 500px; height: 500px; border: 2px solid red; margin-top: 3px;">
+<div class="container">
  
-<h2 class="text-center"> Choose and click to create and view </h2>
+<h2 class="text-center"> Choose, Click Create !</h2>
 <p>
 	<a href="newfamily.php">Create New Family</a>
 </p>
