cdb476bc1c602341a606c7e4c77e6e6724840c0f
Eve Awe <joeverlyncapute@gmail.com>
2021-02-13 17:38:07 -0700

    dsfgadga

--- web/wk06-dbupdateFEPS/newalert.php
+++ web/wk06-dbupdateFEPS/newalert.php
@@ -40,37 +40,35 @@
 
 
 <div> 
-  <form> 
-    <p>First name: <input type="text" id="fname" name ="firstname"></p>
-    <p>Last name: <input type="text" id="lname" name="lastname"></p>
-    <p><input type="button" value="Submit" id="namebutton"></p>
-    <script>
-      // Just get a reference to the element
-      var fname = document.getElementById("fname");
-      var lname = document.getElementById("lname");
-      document.getElementById("namebutton").addEventListener("click", function () { 
-        // Then, use the properties of the (already gotten) elment as you need them:
-        alert('Hello ' + fname.value + " " + lname.value);
-      })
-    </script>
-  </form>
-
-
-      <h3> See list of family events here! </h3>
-      <div>
-          <p>
-        <a href="#">	New Year </a> </br>
-          <a href="#">	Valentines </a> </br>
-          <a href="#">	Labor Day </a> </br>
-          <a href="#">	Independence Day </a> </br>
-          <a href="#">	others.. </a></br>
-
-        </p>
-        <br />
-      </div>
-</div>
+<form> 
+  <p>First name: <input type="text" id="fname" name ="firstname"></p>
+  <p>Last name: <input type="text" id="lname" name="lastname"></p>
+  <p><input type="button" value="Submit" id="namebutton"></p>
+  <script>
+    // Just get a reference to the element
+    var fname = document.getElementById("fname");
+    var lname = document.getElementById("lname");
+    document.getElementById("namebutton").addEventListener("click", function () { 
+      // Then, use the properties of the (already gotten) elment as you need them:
+      alert('Hello ' + fname.value + " " + lname.value);
+    })
+  </script>
+</form>
 </div>
 
+<h3> See list of family events here! </h3>
+<div>
+    <p>
+	 <a href="#">	New Year </a> </br>
+     <a href="#">	Valentines </a> </br>
+     <a href="#">	Labor Day </a> </br>
+     <a href="#">	Independence Day </a> </br>
+     <a href="#">	others.. </a></br>
+
+	</p>
+  <br />
+</div>
+</div>
 
 
 
