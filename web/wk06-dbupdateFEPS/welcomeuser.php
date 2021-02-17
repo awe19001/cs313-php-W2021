1e82d3b686161fe186d7b70a7dd53d4ff9dc63dd
Eve Awe <joeverlyncapute@gmail.com>
2021-02-17 00:47:57 -0700

    af

--- web/wk06-dbupdateFEPS/welcomeuser.php
+++ web/wk06-dbupdateFEPS/welcomeuser.php
@@ -10,6 +10,8 @@
     
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
+You are a: <?php echo $_POST["password"]; ?>
+Your password is: <?php echo $_POST["checkbox"]; ?>
 
 </body>
 </html>
