82ab4de1e57229e097b293c235261727dad6bd81
Eve Awe <joeverlyncapute@gmail.com>
2021-02-17 18:33:00 -0700

    f

---
+++ web/wk05/view.php
@@ -0,0 +1,48 @@
+<?php
+include "dbconnect.php";
+$db = get_db();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Page</title>
+</head>
+<body>
+
+    <div>   
+        <td>users</td>
+
+    <table>
+        <thead>
+            <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Action</th>           
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <br>
+            </tr>
+        
+        </tbody>
+    </table>
+    </div>
+
+    
+</body>
+</html>
