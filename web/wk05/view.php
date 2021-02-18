693282f4ef09ca2733b621d4bc00d58b42c0ca6a
Eve Awe <joeverlyncapute@gmail.com>
2021-02-17 18:59:37 -0700

    sdf

--- web/wk05/view.php
+++ web/wk05/view.php
@@ -1,6 +1,13 @@
 <?php
 include "dbconnect.php";
 $db = get_db();
+
+//write the query to get data from usertracker table
+$sql = "SELECT * FROM usertracker";
+
+//execute the query
+$result = $db->prepare($sql);
+
 ?>
 
 
@@ -10,14 +17,15 @@ $db = get_db();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <title>View Page</title>
 </head>
 <body>
 
-    <div>   
+    <div class="container">   
         <td>users</td>
 
-    <table>
+    <table class="table">
         <thead>
             <tr>
             <th>ID</th>
@@ -29,15 +37,25 @@ $db = get_db();
             </tr>
         </thead>
         <tbody>
-            <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <br>
-            </tr>
+            <?php      
+            if ($result->num_rows = 0) {
+                //output data of each row
+                while ($row = $result()) {
+            ?>
+                    <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['firstname']; ?></td>
+                    <td><?php echo $row['lastname']; ?></td>
+                    <td><?php echo $row['email']; ?></td>
+                    <td><?php echo $row['gender']; ?></td>
+                    <td><a class="btn btn-info" href="update.php?id=<?php echo $row['id']; ?>">Edit</a>&nbsp;<a class="btn btn-danger"href="delete.php?id=<?php echo $row['id']; ?>">Delete</a></td>
+                    </tr>
+
+             <?php }
+            }
+            ?>   
+
+                 
         
         </tbody>
     </table>
