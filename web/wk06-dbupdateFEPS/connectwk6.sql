4bb85216fcb72aca4580e5d7b7ea7ac1f5414a34
awe19001 <59641535+awe19001@users.noreply.github.com>
2021-02-19 20:25:50 -0700

    Add files via upload

---
+++ web/wk06-dbupdateFEPS/connectwk6.sql
@@ -0,0 +1,28 @@
+CREATE TABLE landing (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(25) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL 
+);
+CREATE TABLE notenewfamily (
+    id SERIAL PRIMARY KEY,
+    family_id INT NOT NULL REFERENCES landing(id),
+    content text NOT NULL
+);
+
+INSERT INTO landing(code, name) VALUES ('CNF', 'New Families');
+INSERT INTO landing(code, name) VALUES ('CNU', 'New Users');
+INSERT INTO landing(code, name) VALUES ('CNE', 'New Event for Family');
+INSERT INTO landing(code, name) VALUES ('LEA', 'List of Events and Alert');
+
+INSERT INTO notenewfamily(family_id, content) VALUES (1, 'Family is ordained of God.');
+INSERT INTO notenewfamily(family_id, content) VALUES (2, 'New users are welcome.');
+INSERT INTO notenewfamily(family_id, content) VALUES (3, 'What activity we are going to do as family.');
+INSERT INTO notenewfamily(family_id, content) VALUES (4, 'See the events you have made.');
+
+SELECT * FROM notenewfamily n JOIN landing l ON n.family_id = l.id;
+
+SELECT l.code, l.name, n.content FROM notenewfamily n 
+JOIN landing l ON n.family_id = l.id
+WHERE l.id = 1;
+
+
